20d131552d420d3520d76eb938725fbf8088ceba
dbj <dbj@dbj.org>
2021-01-11 00:39:00 +0100

    update

--- slot/packed_array.h
+++ slot/packed_array.h
@@ -23,51 +23,55 @@ namespace dbj
 	};
 
 	template<
-		typename Object,
-		std::enable_if_t<  std::is_trivially_constructible_v<Object>, int> = 0
+		typename element_type,
+		std::enable_if_t<  std::is_default_constructible_v<element_type>, int > = 0
 	>
 		struct packed_array final
 		:private no_copy_no_move
 	{
 		using type = packed_array;
-		using ID = unsigned;
+		using value_type = element_type;
+		using id_type = unsigned;
 
 		constexpr static size_t MAX_OBJECTS = 64 * 1024;
 		constexpr static size_t INDEX_MASK = 0xffff;
 		constexpr static size_t NEW_OBJECT_ID_ADD = 0x10000; // aka (int)65536
 
 		// "special values" are not excatly a good design, I know
-		constexpr static ID invalid_id = (ID)(-1);
+		constexpr static id_type invalid_id = (id_type)(-1);
+
+		using ushort = unsigned short;
 
 		// aka Slot aka Handle aka Index
-		// author of packed array is not exposing the handle but the ID of the handle
+		// author of packed array is not exposing the handle but the id_type of the handle
 		// that confuses the design slightly
 		// but simplifies the implementation and makes it more resilient
-		// carrying arround ID aka unsigned int imposes no performance 
+		// carrying arround id_type aka unsigned int imposes no performance 
 		// or complexity penalty
 		struct Handle final
 		{
-			// dbj: vs original this is significant improvement
-			//      id is constant
-			//      and since id is (see the ctor) actually the internal arrays index
-			//      we can use that facto for quick and resilient id validity check
-			const ID id{};
-			unsigned short index{};
-			unsigned short next{};
+			id_type id{};
+			ushort index{};
+			ushort next{};
 
-			// do ot add anuthing but data in here as that 
+			// do not add anything but data in here as that 
 			// will make it not a trivial structure aka "not a pod"
 			// and that will provoke compiler to generate large hidden scafolding
 		};
 
 
 		unsigned num_objects_{};
+
+
+		unsigned size() const noexcept {
+			return num_objects_;
+		};
 		// native arrays of Objects are somewhat troublesome in C++
 		// this effectively requires Objects to be trivially constructible
-		Object objects_[MAX_OBJECTS]{};
+		value_type objects_[MAX_OBJECTS]{};
 		Handle handles_[MAX_OBJECTS]{};
-		unsigned short freelist_enqueue_{};
-		unsigned short freelist_dequeue_{};
+		ushort freelist_enqueue_{};
+		ushort freelist_dequeue_{};
 
 		explicit packed_array() noexcept
 		{
@@ -75,14 +79,14 @@ namespace dbj
 			for (unsigned i = 0; i < MAX_OBJECTS; ++i)
 			{
 				// MSVC use of designated initializers requires at least '/std:c++latest'
-				handles_[i] = Handle{ /*.id =*/ i, /*.index =*/ 0, /*.next =*/ i + 1 };
+				handles_[i] = Handle{ /*.id =*/ i, /*.index =*/ ushort(0), /*.next =*/ ushort(i + 1) };
 			}
 			freelist_dequeue_ = 0;
 			freelist_enqueue_ = MAX_OBJECTS - 1;
 		}
 
 		// this effectivley means id is in the range 
-		bool valid_id(ID id) const noexcept {
+		bool valid_id(id_type id) const noexcept {
 
 			// also covers the invalid_id case 
 			// as it is effectively max size_t
@@ -99,12 +103,12 @@ namespace dbj
 
 		// this effectively means Id is in the range AND
 		// it point to something meaningfull
-		bool has(ID id) noexcept
+		bool has(id_type id) noexcept
 		{
 			// va the original design this allso slows 
 			// down things considerably
 			if (valid_id(id)) {
-				// how do we knopw handle is valid?
+				// how do we know if handle is valid?
 				Handle& in = handles_[id & INDEX_MASK];
 				return in.id == id && in.index != USHRT_MAX;
 			}
@@ -114,29 +118,29 @@ namespace dbj
 		// what here?
 		// what is "invalid object" ?
 		// this is an ugly hack
-		static const Object invalid_object{};
+		inline static const value_type invalid_object{};
 
-		bool is_object_valid(Object const& specimen_) const noexcept
+		bool is_object_valid(value_type const& specimen_) const noexcept
 		{
 			return (&specimen_ != &invalid_object);
 		}
 
-		Object& lookup(ID id) noexcept
+		value_type& lookup(id_type id) noexcept
 		{
 			if (has(id)) {
 				return objects_[handles_[id & INDEX_MASK].index];
 			}
-			return static_cast<Object&>(invalid_object);
+			return const_cast<value_type&>(invalid_object);
 		}
 
 		// this is effectively allocation from a stack based array
-		// we know what type is ID so we can copy on return 
-		// if ID would be something more complex we 
+		// we know what type is id_type so we can copy on return 
+		// if id_type would be something more complex we 
 		// might return by reference
 		// 'add' would be nice and chort name 
 		// 'new_element' carries more information and is not too long to type
 		// in case of overflow we return invalid_id
-		ID new_element() noexcept
+		id_type new_element() noexcept
 		{
 			// this is also a release build operation and slows 
 			// down the whole thing
@@ -148,22 +152,23 @@ namespace dbj
 			freelist_dequeue_ = handle_.next;
 			handle_.id += NEW_OBJECT_ID_ADD;
 			handle_.index = num_objects_++;
-			Object& o = objects_[handle_.index];
-			o.id = handle_.id;
-			return o.id;
+			value_type& o = objects_[handle_.index];
+			return handle_.id;
+			//o.id = handle_.id;
+			//return o.id;
 		}
 
-		// return true if ID was valid, false otherwise
+		// return true if id_type was valid, false otherwise
 		// this is a "big deal" and why handles at all
 		// if id would be a pointer it would be imppossible
 		// to tell if it points to the array in here or  is it
 		// "dangling"
 		// unfortunately Alex Stepanov STL has no concept of handles
-		bool remove(ID id)
+		bool remove(id_type id)
 		{
 			Handle& handle_ = handles_[id & INDEX_MASK];
 
-			Object& o = objects_[handle_.index];
+			value_type& o = objects_[handle_.index];
 			o = objects_[--num_objects_];
 			handles_[o.id & INDEX_MASK].index = handle_.index;
 
